6f821b7706f4a4f0fcc9e8d93a9cf54902b1bfe0
xuyuejun <lwxuyuejun@163.com>
2019-06-08 21:32:03 +0800

    Set camera page to self-adaption

--- src/pages/Camera/CameraPage.vue
+++ src/pages/Camera/CameraPage.vue
@@ -1,37 +1,43 @@
 <template>
     <div>
         <el-card>
-            <div>
-                <img v-if="cameraOnline === true" src='http://raspberrypi.local:8080/?action=stream' style="width: 100%" height="auto"/>
-                <img v-if="cameraOnline === false" src='../../images/camera-offline.png' style="width: 100%" height="auto"/>
-            </div>
-            <el-card>
-                <div class="buttonCenter">
-                    <el-button-group>
-                        <el-button v-if="controlModel === 1" type="primary">云台控制</el-button>
-                        <el-button v-if="controlModel !== 1" @click="controlModel = 1">云台控制</el-button>
-                        <el-button v-if="controlModel === 2" type="primary">参数设置</el-button>
-                        <el-button v-if="controlModel !== 2" @click="controlModel = 2">参数设置</el-button>
-                    </el-button-group>
-                </div><br>
-                <div v-if="controlModel === 1">
-                    <div>
-                        <span class="demonstration">水平舵机</span>
-                        <el-slider v-model="PTZ.Horizontal" :max="180" @change="horizontalChange"></el-slider>
-                    </div>
-                    <div>
-                        <span class="demonstration">垂直舵机</span>
-                        <el-slider v-model="PTZ.Vertical" :max="180"></el-slider>
-                    </div>
-                </div>
-                <div v-if="controlModel === 2">
-                    <el-form ref="form" :model="outputPlugin" label-width="80px">
-                        <el-form-item label="在线状态" prop="delivery">
-                            <el-switch v-model="cameraOnline"></el-switch>
-                        </el-form-item>
-                    </el-form>
-                </div>
-            </el-card>
+            <el-row :gutter="10">
+                <el-col class="col-xs-12 col-md-8">
+                    <el-card :body-style="{ padding: '0px' }">
+                        <img v-if="cameraOnline === true" src='http://raspberrypi.local:8080/?action=stream' style="width: 100%" height="auto"/>
+                        <img v-if="cameraOnline === false" src='../../images/camera-offline.png' style="width: 100%" height="auto"/>
+                    </el-card>
+                </el-col>
+                <el-col class="col-xs-6 col-md-4">
+                    <el-card>
+                        <div class="buttonCenter">
+                            <el-button-group>
+                                <el-button v-if="controlModel === 1" type="primary">云台控制</el-button>
+                                <el-button v-if="controlModel !== 1" @click="controlModel = 1">云台控制</el-button>
+                                <el-button v-if="controlModel === 2" type="primary">参数设置</el-button>
+                                <el-button v-if="controlModel !== 2" @click="controlModel = 2">参数设置</el-button>
+                            </el-button-group>
+                        </div><br>
+                        <div v-if="controlModel === 1">
+                            <div>
+                                <span class="demonstration">水平舵机</span>
+                                <el-slider v-model="PTZ.Horizontal" :max="180" @change="horizontalChange"></el-slider>
+                            </div>
+                            <div>
+                                <span class="demonstration">垂直舵机</span>
+                                <el-slider v-model="PTZ.Vertical" :max="180"></el-slider>
+                            </div>
+                        </div>
+                        <div v-if="controlModel === 2">
+                            <el-form ref="form" :model="outputPlugin" label-width="80px">
+                                <el-form-item label="在线状态" prop="delivery">
+                                    <el-switch v-model="cameraOnline"></el-switch>
+                                </el-form-item>
+                            </el-form>
+                        </div>
+                    </el-card>
+                </el-col>
+            </el-row>
         </el-card>
     </div>
 </template>
